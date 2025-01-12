17696945b5ab383022cdb109c14f6052f5dbae00
shane <rshan.dev@gmail.com>
2025-01-12 11:38:02 +0400

    initial commit

---
+++ app/test/layout.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const layout = ({ children }: any) => {
+  return <section className=" bg-white text-yellow-700">{children}</section>
+}
+
+export default layout
---
+++ app/test/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <section>
+      hello world
+    </section>
+  )
+}
+
+export default page
