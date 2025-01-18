b844aaada48b21d599c621636f46f9680c68fd06
shane <rshan.dev@gmail.com>
2025-01-18 23:51:57 +0400

    redirect to dashboard and overview section

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
         >
           <SidebarProvider>
             <AppSidebar />
-            <main>
+            <main className="w-full">
               <SidebarTrigger />
               {children}
             </main>
--- components/mira_component/Overview.tsx
+++ components/mira_component/Overview.tsx
@@ -2,9 +2,23 @@ import React from "react"
 
 const Overview = async () => {
   return (
-    <section>
+    <section className="">
       <div className="container">
-        <h1>Overview</h1>
+        <h1 className="text-3xl text-gray-900 font-bold">Overview</h1>
+        <div className="flex w-full justify-between pt-8 gap-3 flex-col md:flex-row md:gap-6">
+          <div className=" bg-gray-900 text-white py-6 px-6 rounded-sm w-full max-w-[338px]">
+            <span className=" text-sm">Current Balance</span>
+            <span className="block font-bold text-3xl pt-3">$4,836.00</span>
+          </div>
+          <div className=" bg-white text-gray-900 py-6 px-6 rounded-sm w-full max-w-[338px]">
+            <span className=" text-sm">Current Balance</span>
+            <span className="block font-bold text-3xl pt-3">$4,836.00</span>
+          </div>
+          <div className=" bg-white text-gray-900 py-6 px-6 rounded-sm w-full max-w-[338px]">
+            <span className=" text-sm">Current Balance</span>
+            <span className="block font-bold text-3xl pt-3">$4,836.00</span>
+          </div>
+        </div>
       </div>
     </section>
   )
