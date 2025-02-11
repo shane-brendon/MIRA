a88758a3af9160e18a223ebb25033ff5ee65285f
shane <rshan.dev@gmail.com>
2025-02-11 19:01:45 +0400

    rm chart on dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
-import Hero from "@/components/hero"
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps"
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps"
-import { hasEnvVars } from "@/utils/supabase/check-env-vars"
 import { redirect } from "next/navigation"
 import { createClient } from "@/utils/supabase/server"
 
