5c9108c845b485fea41a47f814dac82e68e0ac5e
shane <rshan.dev@gmail.com>
2025-02-11 18:50:21 +0400

    redirect to sign-up

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
-import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+import Hero from "@/components/hero"
+import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps"
+import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps"
+import { hasEnvVars } from "@/utils/supabase/check-env-vars"
+import { redirect } from "next/navigation"
+import { createClient } from "@/utils/supabase/server"
 
 export default async function Home() {
-  return (
-    <>
-      <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-      </main>
-    </>
-  );
+  const supabase = await createClient()
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  if (!user) {
+    return redirect("/sign-in")
+  }
+  return redirect("/dashboard")
 }
